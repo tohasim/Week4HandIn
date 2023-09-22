ab5848a9f01c61f76997b327c506af9956d45b3a
Tohasim <toreh.simonsen@live.dk>
2023-09-22 10:08:56 +0200

    Task 1 done

--- jsDay4/pages/p2/p2.js
+++ jsDay4/pages/p2/p2.js
@@ -1,3 +1,15 @@
+let sharedText = "";
+
 export function initP2() {
   console.log("In initP2()");
+  document.getElementById("btn-save-text").addEventListener("click", saveText);
+}
+
+function saveText() {
+  sharedText = document.getElementById("input-text").value;
+  document.getElementById("text").innerText = sharedText;
+}
+
+export function getText() {
+  return sharedText;
 }
--- jsDay4/pages/p3/p3.js
+++ jsDay4/pages/p3/p3.js
@@ -1,3 +1,6 @@
+import { getText } from "../p2/p2.js";
+
 export function initP3() {
   console.log("In initP3()");
+  document.getElementById("text").innerText = getText();
 }
